2ab981d48b8355d0943e0004b8119a04c5bb317b
Ekaterina Shchegoleva <kateg@list.ru>
2023-07-10 16:20:42 +0300

    feat: добавила вывод распознанной речи на экран

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { SpeechRecognition } from './SpeechRecognition';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <header className="App-header">     
+        <SpeechRecognition/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/SpeechRecognition.jsx
+++ src/SpeechRecognition.jsx
@@ -1,19 +1,49 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import Button from 'react-bootstrap/Button';
+
 export const SpeechRecognition=()=> {
+    const [speech, setSpeech]=useState([])
     const recognition = new (window.SpeechRecognition ||
         window.webkitSpeechRecognition ||
         window.mozSpeechRecognition ||
         window.msSpeechRecognition)();
-      recognition.lang = "ru-RU";
+      recognition.lang = "ru-RU";   
+      recognition.continuous = false;
       recognition.interimResults = true;
-      recognition.continuous = true;
-      recognition.maxAlternatives = 30;
-      // recognition.start();
+      recognition.maxAlternatives = 30;  
       
-      recognition.onresult = function(event) {
-        console.log("You said: ", event.results[0][0].transcript);
-        console.log(event.results);
+      let phrase=[]
+      recognition.onresult = function(event) {  
+        let current = event.resultIndex;
+        window.transcript = event.results[current][0].transcript;
+        let a=Math.floor(Math.random() * 500);
+        phrase[current]=(`${window.transcript} ${a}`)
+        console.log(window.transcript);
+        setSpeech((speech) => [phrase])
       };
+      console.log(speech[0])
+      const startListening=()=> {
+        recognition.continuous = true;
+        recognition.start()
+      }
+    const stopListening=()=> {    
+        recognition.stop()
+        recognition.continuous = false;
+      }
+
+
+
 return (
-    <button onClick={()=>recognition.start()}>Listening</button>
+    <>
+    <div>
+    <Button variant="light" onClick={()=>startListening()}>Начать слушать</Button>
+    <Button style={{margin: "0 10px"}}variant="light" onClick={()=>stopListening()}>Закончить</Button>
+    <Button variant="light" onClick={()=>setSpeech([])}>Очистить</Button>
+    </div>
+    <ul style={{listStyleType: "none", marginTop:"40px"}}>
+    {speech[0]&&speech[0].map(item=><li key={uuidv4()}>{item}</li>)}
+    </ul>
+    </>
 )    
 }
